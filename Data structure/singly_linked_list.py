4d5e492a003f5b4a1f8a3ed43cf9d534ab96d6b1
anrunda9 <ljy6816@gmail.com>
2021-06-12 23:02:16 +0900

    Add add & contains

---
+++ Data structure/singly_linked_list.py
@@ -0,0 +1,39 @@
+from icecream import ic
+
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+class LinkedList(object):
+  def __init__(self):
+    self.head = None
+
+  def add(self, new):
+    new_node = Node(new)
+
+    if self.head:
+      pointer = self.head
+      while pointer.next:
+        pointer = pointer.next
+      pointer.next = new_node
+    else:
+      self.head = new_node
+  
+  def contains(self, obj):
+    search = self.head
+    while search:
+      if search.data == obj:
+        return True
+      search = search.next
+    return False
+  
+  
+a = LinkedList()
+# print(a, a.head)
+a.add(5)
+# print(a.head, a.head.data, a.head.next)
+a.add(10)
+# print(a.head, a.head.data, a.head.next)
+a.add(15)
+print(a.contains(15))
